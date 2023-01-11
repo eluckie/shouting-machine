0bf1d53dff15c8fd4ebdc10740348c00def793e0
E Luckie <eluckie.d@gmail.com>
2023-01-11 12:15:05 -0700

    add scripts.js function to return user input to user as capital.

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+window.onload = function () {
+  let form = document.querySelector("form");
+  form.onsubmit = function(event) {
+    event.preventDefault();
+
+    const textInput = document.getElementById("textInput").value.toUpperCase();
+
+    document.querySelector("span#input").innerText = textInput;
+
+    document.querySelector("div#shout").removeAttribute("class");
+  };
+};
